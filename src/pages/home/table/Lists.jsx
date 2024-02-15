87620b5dca606841d3f3ff595d66ea2c975d96e9
256bitguy <ashishrajsri2233@gmail.com>
2024-02-15 11:15:11 +0530

    tablebody working on local json file ,fetching part is remaining

--- src/pages/home/table/Lists.jsx
+++ src/pages/home/table/Lists.jsx
@@ -3,6 +3,56 @@ import { Button,   Table, TableBody, TableCell, TableContainer, TableHead, Table
 
 function Lists() {
   const [data,setData]=useState();
+  const Daa=[
+    {"date":"6/02/2024",
+     "react":"Updated Work",
+       "node":"No progress",
+       "apply":"Link click here",
+       "others":"Nothing"
+    },
+    {"date":"7/02/2024",
+     "react":"Updated Work",
+       "node":"No progress",
+       "apply":"Link click here",
+       "others":"Nothing"
+    },
+    {"date":"8/02/2024",
+     "react":"Updated Work",
+       "node":"No progress",
+       "apply":"Link click here",
+       "others":"Nothing"
+    },
+    {"date":"9/02/2024",
+     "react":"Updated Work",
+       "node":"No progress",
+       "apply":"Link click here",
+       "others":"Nothing"
+    },
+    {"date":"10/02/2024",
+     "react":"Updated Work",
+       "node":"No progress",
+       "apply":"Link click here",
+       "others":"Nothing"
+    },
+    {"date":"11/02/2024",
+     "react":"Updated Work",
+       "node":"No progress",
+       "apply":"Link click here",
+       "others":"Nothing"
+    },
+    {"date":"12/02/2024",
+     "react":"Updated Work",
+       "node":"No progress",
+       "apply":"Link click here",
+       "others":"Nothing"
+    },
+    {"date":"13/02/2024",
+     "react":"Updated Work",
+       "node":"No progress",
+       "apply":"Link click here",
+       "others":"Nothing"
+    }
+ ]
   useEffect(()=>{
    const  result=async ()=>{
       try{
@@ -11,6 +61,7 @@ function Lists() {
           throw new Error('Network response was not ok');
         }
         const result = await response.json();
+        console.log(result);
         setData(result);
     }catch (error){
       console.log(error)
@@ -18,7 +69,8 @@ function Lists() {
     }
     result();
   },[])
-  console.log(data);
+  console.log(Daa);
+  const row=["Date","ReactJs","NodeJS","Apply","Others"];
   return (
     <div><div style={{
         width:"100vw",
@@ -28,23 +80,33 @@ function Lists() {
           <Table>
             <TableHead>
               <TableRow>
-                <TableCell style={{fontSize:"20px",textAlign:"center",border:"1px solid black",margin:'2px'}}>Date</TableCell>
-                <TableCell style={{fontSize:"20px",textAlign:"center",border:"1px solid black",margin:'2px'}}>ReactJs</TableCell>
-                <TableCell style={{fontSize:"20px",textAlign:"center",border:"1px solid black",margin:'2px'}}>NodeJS</TableCell>
-                <TableCell style={{fontSize:"20px",textAlign:"center",border:"1px solid black",margin:'2px'}}>Apply</TableCell>
-                <TableCell style={{fontSize:"20px",textAlign:"center",border:"1px solid black",margin:'2px'}}>Others</TableCell>
+                {
+                  row.map((item)=>{
+                    return(
+                      <TableCell style={{fontSize:"20px",textAlign:"center",border:"1px solid black",margin:'2px'}}>{item}</TableCell>
+                    )
+                  })
+                }
+                
                  
               </TableRow>
             </TableHead>
             <TableBody>
-              <TableRow  >
-                <TableCell style={{fontSize:"20px",textAlign:"center",border:"1px solid black",margin:'2px'}}>12/02/2024</TableCell>
-                <TableCell style={{fontSize:"20px",textAlign:"center",border:"1px solid black",margin:'2px'}}>Updated last project</TableCell>
-                <TableCell style={{fontSize:"20px",textAlign:"center",border:"1px solid black",margin:'2px'}}>No Progress</TableCell>
-                <TableCell style={{fontSize:"20px",textAlign:"center",border:"1px solid black",margin:'2px'}}><a>click here!!</a></TableCell>
-                <TableCell style={{fontSize:"20px",textAlign:"center",border:"1px solid black",margin:'2px'}}>interview tha</TableCell>
-              </TableRow>
-            </TableBody>
+             {
+              Daa.map((item)=>{
+                return(
+                  <TableRow  >
+                      <TableCell style={{fontSize:"20px",textAlign:"center",border:"1px solid black",margin:'2px'}}>{item.date}</TableCell>
+                      <TableCell style={{fontSize:"20px",textAlign:"center",border:"1px solid black",margin:'2px'}}>{item.react}</TableCell>
+                      <TableCell style={{fontSize:"20px",textAlign:"center",border:"1px solid black",margin:'2px'}}>{item.node}</TableCell>
+                      <TableCell style={{fontSize:"20px",textAlign:"center",border:"1px solid black",margin:'2px'}}> {item.apply}</TableCell>
+                      <TableCell style={{fontSize:"20px",textAlign:"center",border:"1px solid black",margin:'2px'}}> {item.others}</TableCell>
+                      </TableRow>
+                      
+                      )
+                    })
+                  }
+                  </TableBody>
           </Table>
         </TableContainer>
       </div></div>
